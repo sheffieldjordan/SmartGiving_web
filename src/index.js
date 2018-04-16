a57715bab83645be223d8c49a7243a30667d73de
Gabe Nicholas <gabe.nicholas@gmail.com>
2018-04-15 22:54:51 -0700

    Unit tests now pass

--- src/index.js
+++ src/index.js
@@ -3,29 +3,10 @@ import ReactDOM from 'react-dom';
 import './style/Components.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-
-import { createStore } from 'redux'
-import {updateDrawer} from './redux/reducers'
-import data from './data/user'
-import requests from './data/requests'
-
-
+import {store} from './RootStore'
 
 
 window.React = React
-
-const initialState = {
-	donationDrawerOpen: false,
-	selectedRequest: {},
-	...data,
-	...requests
-}
-
-const store = createStore(
-	updateDrawer,
-	initialState
-)
 
 
 ReactDOM.render(
