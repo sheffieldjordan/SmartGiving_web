9ebe69e3262dd611b1bed5aa6f2093b1c7a503f2
Gabe Nicholas <gabe.nicholas@gmail.com>
2018-04-21 14:01:27 -0700

    Added merchant request making section

--- src/containers/CreateRequest.js
+++ src/containers/CreateRequest.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 import TabBar from '../components/TabBar'
 import NavBar from '../components/NavBar'
 import DescribeGift from '../components/DescribeGift'
-
+import ItemizeGift from '../components/ItemizeGift'
 class CreateRequest extends Component {
 
 	render() {
 		const displayData = {
 			"Basic Information" : <DescribeGift/>,
-			"List of Goods" : <div>Two</div>,
-			"Merchant Selection" : <div>Three</div>
+			"List of Goods" : <ItemizeGift store={this.props.store}/>,
+			"Merchant Selection" : <div>Maybe implement this later?</div>
 		}
 		return (
 			<div>
