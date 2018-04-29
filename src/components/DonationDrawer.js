bf628b1ef40199cd653af883ea75c92f6732bd34
Morgan <Morgan@The-Jordans-MacBook-Pro.local>
2018-04-29 06:19:11 -0700

    DonationDrawer updates price dynamically

--- src/components/DonationDrawer.js
+++ src/components/DonationDrawer.js
@@ -1,46 +1,101 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react"
+import PropTypes from "prop-types"
 
-import Drawer from 'material-ui/Drawer';
-import Button from 'material-ui/Button';
+import Drawer from "material-ui/Drawer"
+import Button from "material-ui/Button"
 
-import UserAvatar from './UserAvatar';
-import {DollarsToEther} from '../style/Formatter'
+import UserAvatar from "./UserAvatar"
+import { DollarsToEther } from "../style/Formatter"
+import price from "crypto-price"
 
 class DonationDrawer extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			ethRate: ""
+		}
+	}
+
+	componentDidMount() {
+		const request =
+			this.props.charity.gifts !== undefined ? this.props.charity.gifts[0] : {}
+		const donationValue =
+			this.props.donationValue === undefined
+				? request.dollars
+				: this.props.donationValue
+		price
+			.getCryptoPrice("USD", "ETH")
+			.then((obj) => {
+				var etherCut = parseFloat(obj.price).toFixed(2)
+				var rate = donationValue / etherCut
+				this.setState({ ethRate: rate.toFixed(3) })
+			})
+			.catch((err) => {
+				console.log(err)
+			})
+	}
 
 	render() {
-		const request = this.props.charity.gifts !== undefined ? this.props.charity.gifts[0] : {}
+		const request =
+			this.props.charity.gifts !== undefined ? this.props.charity.gifts[0] : {}
 		const charity = this.props.charity
-		const donationValue = this.props.donationValue === undefined ? request.dollars : this.props.donationValue
+		const donationValue =
+			this.props.donationValue === undefined
+				? request.dollars
+				: this.props.donationValue
+
 		return (
-		<Drawer anchor="bottom" open={this.props.data.open} onClose={this.props.data.onClose}>
-			<div className="drawer-container">
-			<h1 className = "drawer-title">Confirm Donation</h1>
-				<div className="donate-avatar-container">
-					<div className = "donate-avatar-container">
-						<UserAvatar displayName="Donor" />
-						<div className = "donate-arrow">&#8680;</div>
-						<UserAvatar displayName="Recipient" />
+			<Drawer
+				anchor="bottom"
+				open={this.props.data.open}
+				onClose={this.props.data.onClose}
+			>
+				<div className="drawer-container">
+					<h1 className="drawer-title">Confirm Donation</h1>
+					<div className="donate-avatar-container">
+						<div className="donate-avatar-container">
+							<UserAvatar displayName="Donor" />
+							<div className="donate-arrow">&#8680;</div>
+							<UserAvatar displayName="Recipient" />
+						</div>
+					</div>
+					<div className="drawer-description donate-pre-description">
+						Are you sure you would like to donate ${Math.floor(
+							donationValue
+						).toFixed(2)}{" "}
+						({this.state.ethRate} ETH) to {charity.title}?
+					</div>
+					<div className="drawer-description donate-what-happens">
+						(Your payment will be processed once a merchant and recipient agree
+						on delivering and receiving your gifts before the expiration date.)
+					</div>
+					<div className="drawer-button-container">
+						<Button
+							className="drawer-button"
+							onClick={this.props.data.onPrimary}
+							variant="raised"
+							size="medium"
+							color="primary"
+						>
+							Donate
+						</Button>
+						<Button
+							className="drawer-button"
+							onClick={this.props.data.onSecondary}
+							variant="raised"
+							size="medium"
+							color="default"
+						>
+							Cancel
+						</Button>
+					</div>
+					<div className="drawer-description donate-post-description">
+						If you choose to donate, you will be brought to MetaMask where you
+						can complete your donation.
 					</div>
 				</div>
-				<div className = "drawer-description donate-pre-description">
-				Are you sure you would like to donate ${Math.floor(donationValue).toFixed(2)} ({DollarsToEther(donationValue)} ETH) to {charity.title}?
-				</div>
-				<div className = "drawer-description donate-what-happens">
-				(Your payment will be processed once a merchant and recipient agree on delivering and receiving your gifts before the expiration date.)
-				</div>
-				<div className = "drawer-button-container">
-					<Button className = "drawer-button" onClick = {this.props.data.onPrimary} variant="raised" size="medium" color="primary">Donate</Button>
-					<Button className = "drawer-button" onClick = {this.props.data.onSecondary} variant="raised" size="medium" color="default">Cancel</Button>
-				</div>
-				<div className = "drawer-description donate-post-description">
-				If you choose to donate, you will be brought to MetaMask where you can complete your donation.
-				</div>
-
-			</div>
-		</Drawer>
-
+			</Drawer>
 		)
 	}
 }
@@ -51,20 +106,18 @@ DonationDrawer.propTypes = {
 		open: PropTypes.bool,
 		onClose: PropTypes.func,
 		onPrimary: PropTypes.func, // When the user hits the primary butotn
-		onSecondary: PropTypes.func, // When the user hits the secondary button
+		onSecondary: PropTypes.func // When the user hits the secondary button
 	})
 }
 
 // WE NEED THIS NOT TO BREAK THE HOMEPAGE
 DonationDrawer.defaultProps = {
 	request: {
-		dollars: 15.0,
+		dollars: 15.0
 	},
 	charity: {
 		title: "SAMPLE CHARITY"
 	}
 	// TODO @Gabe get some default images for here
 }
-export default DonationDrawer;
-
-
+export default DonationDrawer
--- src/style/Formatter.js
+++ src/style/Formatter.js
@@ -1,28 +1,37 @@
 export const StringFromDate = (a) => {
 	// Shout out to StackOverflow
 	// https://stackoverflow.com/a/6078873/1031615
-	
-	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-	var year = a.getFullYear();
-	var month = months[a.getMonth()];
-	var date = a.getDate();
+
+	var months = [
+		"Jan",
+		"Feb",
+		"Mar",
+		"Apr",
+		"May",
+		"Jun",
+		"Jul",
+		"Aug",
+		"Sep",
+		"Oct",
+		"Nov",
+		"Dec"
+	]
+	var year = a.getFullYear()
+	var month = months[a.getMonth()]
+	var date = a.getDate()
 
 	return `${month} ${date}, ${year}`
 }
 
 export const StringFromLocation = (location) => {
-	if (location === undefined) return ''
-	return `${location.address}, ${location.city}, ${location.state}, ${location.country}`
-}
-
-export const DollarsToEther = (dollars) => {
-	const dollarsPerEther = 421.0 // TODO: Get this price dynamically
-	return (parseFloat(dollars)/dollarsPerEther).toFixed(5)
+	if (location === undefined) return ""
+	return `${location.address}, ${location.city}, ${location.state}, ${
+		location.country
+	}`
 }
 
 export const PriceForGift = (gift) => {
 	return gift.reduce((total, currentItem) => {
 		return total + currentItem.price * currentItem.num
 	}, 0)
-
 }
