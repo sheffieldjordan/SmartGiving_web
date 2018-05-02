e5689f745509bc92847001cb00c4a8cbb3505ab1
Gabe Nicholas <gabe.nicholas@gmail.com>
2018-05-01 22:02:40 -0700

    Initially calling ItemSent

--- src/ethereum/components/ItemSent.js
+++ src/ethereum/components/ItemSent.js
@@ -1,44 +1,28 @@
-import React, { Component } from 'react'
 import web3 from '../web3'
 import SmartGift from '../smartgift'
+import {objectContainsKeys} from '../../components/Helpers'
 
-class ItemSent extends Component {
-	state = {
-		errorMessage: ''
-	}
+export const ItemSent = async (ethData, completion) => {
+	try {
 
-	onSubmit = async (event) => {
-		event.preventDefault()
+		const requiredKeys = ['giftAddress']
+		const keyError = objectContainsKeys(ethData, requiredKeys)
+		if (keyError !== undefined)  return completion(keyError)
 
-		try {
-			const targetGift = SmartGift('0xd16038d71B68E149B9441dcEEf6C9c8b339701a6') // address of the Gift you're working on
-			const accounts = await web3.eth.getAccounts()
-			const sentResult = await targetGift.methods.merchantShipsItem().send({
-				from: accounts[0],
-				gas: 1000000
-			})
-			if (sentResult.status === "0x0" || !sentResult.status) {
-				console.log("Transaction Failed!!!")
-			} else {
-				console.log(`You've announced that your item has been sent!`)
-				alert('Success!')
-
-			}
-		} catch (err) {
-			this.setState({ errorMessage: err.message })
+		const targetGift = SmartGift(ethData.giftAddress) // address of the Gift you're working on
+		const accounts = await web3.eth.getAccounts()
+		const sentResult = await targetGift.methods.merchantShipsItem().send({
+			from: accounts[0],
+			gas: 2000000
+		})
+		if (sentResult.status === "0x0" || !sentResult.status) {
+			const err = new Error("Item Sent transaction failed")
+			return completion(err)
+		} else {
+			console.log(`You've announced that your item has been sent!`)
+			return completion()
 		}
-	}
-
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.onSubmit}>
-					<button>Mark Item 'SENT'</button>
-				</form>
-				<h1>{this.state.errorMessage}</h1>
-			</div>
-		)
+	} catch (err) {
+		completion(err)
 	}
 }
-
-export default ItemSent
